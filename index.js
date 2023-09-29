f565b2f872d983e84620065b0a74889ed46dc372
JVPrice <julionprice@hotmail.com>
2023-09-29 09:50:22 +0900

    Add debug and fix reference error

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 4001;
+
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
 
-let users = [{username: "Joe Schmoe", password: "secret123", id: 1},
-{username: "Jill Schmill", password: "secret456", id: 2}
+let carts = [{ userId: 1, products: [] }];
+
+let products = [{}];
+
+app.get("/users/:id", (req, res) => {
+  const userId = Number(req.params.id);
+  const requestedUser = users.find((user) => user.id === userId);
+  res.send(requestedUser);
+});
+
+let users = [
+  { username: "Joe Schmoe", password: "secret123", id: 1 },
+  { username: "Jill Schmill", password: "secret456", id: 2 },
 ];
 
-// let carts = [{userId: 1, products: []}];
+let games = [];
 
-// let products = [{}];
-
-app.get('/users/:id', (req, res) => {
-    const userId = Number(req.params.id);
-    const requestedUser = users.find((user) => user.id === userId);
-    res.send(requestedUser)
+app.get("/games/:id", (req, res) => {
+  const gameId = Number(req.params.id);
+  const requestedGame = games.find((game) => game.id === gameId);
+  res.send(requestedGame);
 });
 
-let users = [{username: "Joe Schmoe", password: "secret123", id: 1},
-{username: "Jill Schmill", password: "secret456", id: 2}
-];
+// app.put();
 
-// let carts = [{userId: 1, products: []}];
+// app.post();
 
-// let games = [{}];
-
-app.get('/users/:id', (req, res) => {
-    const userId = Number(req.params.id);
-    const requestedUser = users.find((user) => user.id === userId);
-    res.send(requestedUser);
-});
-
-app.get('/games/:id', (req,res) => {
-    const gameId = Number(req.params.id);
-    const requestedGame = games.find((game) => game.id === gameId);
-    res.send(requestedGame);
-});
-
-app.put();
-
-app.post();
-
-app.delete();
+// app.delete();
